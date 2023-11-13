907a9ab91c0104d6bef960a751da5a08961d77c5
FrankGrillo1991 <fsg12491@aol.com>
2023-11-12 20:35:09 -0500

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var FirstName = document.querySelector('#fname');
+var LastName = document.querySelector('#lname');
+var Email = document.querySelector('#email');
+var Password = document.querySelector('#password');
+var Default = document.querySelector('button');
