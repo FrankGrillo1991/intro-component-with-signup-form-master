6bcc5983eae618c3fbbe1cd609bbf6597dea1649
FrankGrillo1991 <fsg12491@aol.com>
2023-11-12 20:41:41 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ var LastName = document.querySelector('#lname');
 var Email = document.querySelector('#email');
 var Password = document.querySelector('#password');
 var Default = document.querySelector('button');
+
+
+Default.addEventListener('click', function(e) {
+    e.preventDefault();
+    formValidation();
+});
+
+function formValidation() {
+    if (FirstName.value.trim()=== "") {
+        errorValidation(FirstName, '<em>First Name cannot be empty</em>');
+    } else {
+        successValidation(FirstName);
+    }
+
+}
